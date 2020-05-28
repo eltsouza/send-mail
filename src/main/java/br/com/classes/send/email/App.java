b233254982e7f550bb78beccd843e436356afa3f
eltsouza <tonton_elton@hotmail.com>
2020-05-28 18:50:35 -0300

    Primeiro commit projeto Send-email com com Javax Mail

---
+++ src/main/java/br/com/classes/send/email/App.java
@@ -0,0 +1,13 @@
+package br.com.classes.send.email;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/com/classes/send/email/AppTest.java
@@ -0,0 +1,65 @@
+package br.com.classes.send.email;
+
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class AppTest {
+	
+	private String userName = "souzaconsuloria@gmail.com";
+	private String senha= "java@1985";
+	
+	//Anotação para executar o código
+	@org.junit.Test
+	public void testeEmail() {
+		
+		try {
+			//Verificar as configurações de SMTP do email utilizado
+			Properties properties = new Properties();
+			properties.put("mail.smtp.auth", "true");//autorização
+			properties.put("mail.smtp.starttls", "true");//autenticação
+			properties.put("mail.smtp.host", "smtp.gmail.com");//servidor de email
+			properties.put("mail.smtp.port", "465");//porta do servidor de email
+			properties.put("mail.smtp.socketFactory.port", "465");//Especifica a porta acessada pelo socket
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");//Classe socket de conexão ao SMTP
+		
+			Session session = Session.getInstance(properties, new Authenticator() {
+				
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, senha);
+				}
+			});
+			
+			Address[] toUser = InternetAddress.parse("souza.elton28@gmail.com,souzaconsuloria@gmail.com");
+			//Address[] toUser = InternetAddress.parse("souzaconsuloria@gmail.com");
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(userName)); /*Quem está enviano*/
+			message.setRecipients(Message.RecipientType.TO, toUser);/*Email de destino*/
+			message.setSubject("Chegou e-mail enviado com java");/*Assunto do e-mail*/
+			message.setText("Olá programador, vc acaba de receber um e-mail enviado com Java do curso Formação Java Web do Alex");
+			
+			
+			Transport.send(message);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+			
+		
+		
+	}
+	
+	
+	
+}
